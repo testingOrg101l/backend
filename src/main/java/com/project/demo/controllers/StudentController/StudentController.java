8b7141d4a1c70cfdf4057b5ad3258ae06c72cd02
Haythem532002 <khiarih06@gmail.com>
2025-03-27 17:35:58 +0100

    fix some typos

--- src/main/java/com/project/demo/controllers/StudentController/StudentController.java
+++ src/main/java/com/project/demo/controllers/StudentController/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentService service;
